cc3a3fea45cb861e581ecf34af048a851c451366
AnkeatOpenWindow <221202@virtualwindow.co.za>
2023-10-11 18:10:32 +0200

    Update to adminProducts

--- client/src/components/adminProduct.js
+++ client/src/components/adminProduct.js
@@ -133,7 +133,7 @@ function AdminProduct() {
           </Col>
 
           <Col xs={1} className='text-center'>
-            <p>{product.price}</p>
+            <p>R {product.price}</p>
           </Col>
 
           <Col xs={1} className='text-end'>
